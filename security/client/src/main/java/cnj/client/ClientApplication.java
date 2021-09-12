a57f557891e5a96e006d464d8ddb4d1816a95be0
Josh Long <josh@joshlong.com>
2021-09-12 16:33:02 -0700

    security

--- security/client/src/main/java/cnj/client/ClientApplication.java
+++ security/client/src/main/java/cnj/client/ClientApplication.java
@@ -1,16 +1,25 @@
 package cnj.client;
 
+import io.rsocket.metadata.WellKnownMimeType;
 import org.springframework.boot.ApplicationRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.rsocket.messaging.RSocketStrategiesCustomizer;
 import org.springframework.context.annotation.Bean;
 import org.springframework.core.ParameterizedTypeReference;
 import org.springframework.messaging.rsocket.RSocketRequester;
+import org.springframework.security.rsocket.metadata.SimpleAuthenticationEncoder;
+import org.springframework.security.rsocket.metadata.UsernamePasswordMetadata;
+import org.springframework.util.MimeType;
+import org.springframework.util.MimeTypeUtils;
 
 import java.util.Map;
 
 @SpringBootApplication
 public class ClientApplication {
+
+	private final UsernamePasswordMetadata credentials = new UsernamePasswordMetadata("jlong", "pw");
+	private final MimeType mimeType = MimeTypeUtils.parseMimeType(WellKnownMimeType.MESSAGE_RSOCKET_AUTHENTICATION.getString());
 
 	public static void main(String[] args) throws Exception {
 		SpringApplication.run(ClientApplication.class, args);
@@ -18,31 +27,29 @@ public class ClientApplication {
 	}
 
 	@Bean
+	RSocketStrategiesCustomizer strategiesCustomizer() {
+		return strategies -> strategies.encoder(new SimpleAuthenticationEncoder());
+	}
+
+	@Bean
 	ApplicationRunner requestResponse(RSocketRequester rSocketRequester) {
 		return args -> {
 			var replyStream = rSocketRequester
-				.route("hello.{name}", "RSocket")
+				.route("hello", "RSocket")
+				.metadata(this.credentials, this.mimeType)
 				.retrieveMono(new ParameterizedTypeReference<Map<String, String>>() {
 				});
 			var reply = replyStream.block();
-			System.out.println("hello.{name}=" + reply);
+			System.out.println("hello (authenticated)=" + reply);
 		};
 	}
 
-	@Bean
-	ApplicationRunner stream(RSocketRequester rSocketRequester) {
-		return args -> {
-			var replyStream = rSocketRequester
-				.route("hellos")
-				.retrieveFlux(new ParameterizedTypeReference<Map<String, String>>() {
-				});
-			replyStream.subscribe(r -> System.out.println("hellos=" + r));
-			;
-		};
-	}
 
 	@Bean
-	RSocketRequester rSocketRequester(RSocketRequester.Builder builder) {
-		return builder.tcp("localhost", 8181);
+	RSocketRequester rSocketRequester(
+		RSocketRequester.Builder builder) {
+		return builder
+			.setupMetadata(this.credentials, this.mimeType)
+			.tcp("localhost", 8181);
 	}
 }
--- security/service/src/main/java/cnj/service/ServiceApplication.java
+++ security/service/src/main/java/cnj/service/ServiceApplication.java
@@ -2,13 +2,22 @@ package cnj.service;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.messaging.handler.annotation.DestinationVariable;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
 import org.springframework.messaging.handler.annotation.MessageMapping;
+import org.springframework.messaging.rsocket.RSocketStrategies;
+import org.springframework.messaging.rsocket.annotation.support.RSocketMessageHandler;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.rsocket.RSocketSecurity;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.security.core.userdetails.MapReactiveUserDetailsService;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.messaging.handler.invocation.reactive.AuthenticationPrincipalArgumentResolver;
+import org.springframework.security.rsocket.core.PayloadSocketAcceptorInterceptor;
 import org.springframework.stereotype.Controller;
-import reactor.core.publisher.Flux;
 import reactor.core.publisher.Mono;
 
-import java.time.Duration;
 import java.util.Collections;
 import java.util.Map;
 
@@ -16,23 +25,41 @@ import java.util.Map;
 @SpringBootApplication
 public class ServiceApplication {
 
-	// rsc tcp://localhost:8181 --route hello.rsocketx
-	@MessageMapping("hello.{name}")
-	Mono<Map<String, String>> requestResponse(@DestinationVariable String name) {
-		return Mono.just(Collections.singletonMap("greetings", "Hello, " + name + "!"));
-	}
 
-	// rsc tcp://localhost:8181 --route hellos --stream
-	@MessageMapping("hellos")
-	Flux<Map<String, String>> stream() {
-		return Flux
-			.just("Srinivas", "Zhouyue", "Mario", "Shahram", "Zhen", "Mia", "Valeria")
-			.map(name -> Collections.singletonMap("greetings", "Hello, " + name + "!"))
-			.delayElements(Duration.ofSeconds(1));
+	@MessageMapping("hello")
+	Mono<Map<String, String>> requestResponse(@AuthenticationPrincipal Mono<UserDetails> user) {
+		return user
+			.map(UserDetails::getUsername)
+			.map(name -> Collections.singletonMap("greetings", "Hello, " + name + "!"));
 	}
 
 	public static void main(String[] args) {
 		SpringApplication.run(ServiceApplication.class, args);
 	}
 
+}
+
+@Configuration
+class SecurityConfiguration {
+
+	@Bean
+	PayloadSocketAcceptorInterceptor interceptor(RSocketSecurity security) {
+		return security
+			.simpleAuthentication(Customizer.withDefaults())
+			.authorizePayload(ap -> ap.anyExchange().authenticated())
+			.build();
+	}
+
+	@Bean
+	MapReactiveUserDetailsService authentication() {
+		return new MapReactiveUserDetailsService(User.withDefaultPasswordEncoder().username("jlong").password("pw").roles("USER").build());
+	}
+
+	@Bean
+	RSocketMessageHandler messageHandler(RSocketStrategies strategies) {
+		var rmh = new RSocketMessageHandler();
+		rmh.getArgumentResolverConfigurer().addCustomResolver(new AuthenticationPrincipalArgumentResolver());
+		rmh.setRSocketStrategies(strategies);
+		return rmh;
+	}
 }
